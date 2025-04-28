62a8075e9d7da7561214180353673a474e7dfd71
nattapong2005 <azzzx30089@gmail.com>
2025-04-28 17:27:06 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <h1 className='text-4xl font-bold text-center p-10'>Likeshop Mall</h1>
+    </div>
+  )
+}
+
+export default App
